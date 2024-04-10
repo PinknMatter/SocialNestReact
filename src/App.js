3fac67edeaf1819b17b206d72f180d4552cac8cb
rebecca <113136007+0010beck@users.noreply.github.com>
2024-04-10 14:17:23 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Landing from "./components/LandingPage";
+import Input from "./components/InputPage";
+import Browse from "./components/BrowsePage";
+
+import "./App.css";
+import { useState } from "react";
+import { OpenNav } from "./components/Sidebar";
+import { CloseNav } from "./components/Sidebar";
+
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  const [menuSelector, setMenuSelector] = useState(0);
+
+  function onEnterClick() {
+    setMenuSelector(1);
+  }
+
+  function onSubmitClick() {
+    setMenuSelector(2);
+  }
+
+  if (menuSelector === 0) {
+    return (
+      <div>
+        <Landing onEnterClick={onEnterClick} />
+      </div>
+    );
+  } else if (menuSelector === 1) {
+    return (
+      <div className="slide-up">
+        <Input onSubmitClick={onSubmitClick} />
+      </div>
+    );
+  } else if (menuSelector === 2) {
+    return (
+      <div>
+        <Browse onEnterClick={onEnterClick} />
+      </div>
+    );
+  }
+}
+
+export default App;
